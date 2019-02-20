77af63cac510213f1bef6a379af7999f341db3e0
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-02-20 10:42:32 -0300

    Refactor apply_discount function

--- service01_discount/hashtest.py
+++ service01_discount/hashtest.py
@@ -5,30 +5,13 @@ import hashtest_pb2, hashtest_pb2_grpc
 
 
 class Hashtest(hashtest_pb2_grpc.DiscountServicer):
-    def ApplyDiscount(self, request, content):
-        # TODO -> Refactor discount validation to another function
-
-        DATE_FORMAT = "%d/%m"
-        BLACK_FRIDAY = datetime.datetime(2019, 11, 25).strftime(DATE_FORMAT)
-        MAX_DISCOUNT = decimal.Decimal(10) / 100  # 10%
-
-        user = request.user
+    def apply_discount(self, request, content):
         product = request.product
         discount = hashtest_pb2.DiscountValue()
-
-        # REFACTOR
-        today = datetime.datetime.now().strftime(DATE_FORMAT)
-        user_aniversary = user.date_of_birth.strftime(DATE_FORMAT)
-
-        if today == BLACK_FRIDAY:
-            percentual = decimal.Decimal(10) / 100  # 10%
-        elif today == user_aniversary:
-            percentual = decimal.Decimal(5) / 100  # 5%
-
-        percentual = 0.1 if percentual > MAX_DISCOUNT else percentual
-        # END
+        user = request.user
 
         # TODO -> Find better way to calculate percentages
+        percentual = calculate_discount(user)
         price = decimal.Decimal(product.price_in_cents) / 100
         new_price = price - (price * percentual)
         value_in_cents = int(new_price * 100)
@@ -43,3 +26,22 @@ class Hashtest(hashtest_pb2_grpc.DiscountServicer):
             discount=discount)
 
         return hashtest_pb2.DiscountResponse(product=new_product)
+
+        def calculate_discount(user):
+            DATE_FORMAT = "%d/%m"
+            BLACK_FRIDAY = datetime.datetime(2019, 11, 25).strftime(DATE_FORMAT)
+            today = datetime.datetime.now().strftime(DATE_FORMAT)
+            user_aniversary = user.date_of_birth.strftime(DATE_FORMAT)
+            
+            MAX_DISCOUNT = decimal.Decimal(10) / 100    # 10%
+            percentual = 0
+            
+
+            if today == BLACK_FRIDAY:
+                percentual = decimal.Decimal(10) / 100  # 10%
+            elif today == user_aniversary:
+                percentual = decimal.Decimal(5) / 100   # 5%
+
+            percentual = 0.1 if percentual > MAX_DISCOUNT else percentual
+            
+            return percentual
