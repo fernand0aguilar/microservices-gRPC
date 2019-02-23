8fe84288b67bffda79bef93934092342260cda7e
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2019-02-23 19:52:10 -0300

    Create generate.sh

--- generate.sh
+++ generate.sh
@@ -1,1 +1,8 @@
+echo "Generating py proto files"
+cd service01_discount
 python -m grpc_tools.protoc -I=.. --python_out=. --grpc_python_out=. ../hashtest.proto
+
+echo "Generating go proto files"
+cd ../service02_products_listing
+mkdir hashtest
+protoc -I=.. --go_out=plugins=grpc:hashtest ../hashtest.proto
